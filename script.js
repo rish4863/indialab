1389cdf542eba57149b83f37a60928ac5c928075
Rishi <85361929+rish4863@users.noreply.github.com>
2022-12-04 14:09:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,114 @@
-// java code for getting output
 $(document).ready(function () {
   $("#greetingOutput").text("");
 
   $("#sendName").click(function () {
-     
+    //console.log($("#fname").val());
+   
+    $("#greetingOutput").text("Hello " + $("#fname").val() +" "+ $("#lname").val() +" Your email address  and your birthdate are: "+ $ ("#emailaddress").val()+  " "+ $("#dateofbirth").val()  +  " Your level is: "+ $("#level").val()+" Today's date and time is: "+ $("#datetime").val() + " You were born before 1980: "+ $("#datemax").val() );
+    // $(function() {
 
-  $("#greetingOutput").text("Hello " + $("#fname").val() +" "+ $("#lname").val() +" Your email address  and your birthdate are: "+ $ ("#emailaddress").val()+  " "+ $("#dateofbirth").val()  + " Today's date and time is: "+ $("#datetime").val() + " You have been bowling for : "+ $("#Number").val()+" years" );
 
+$('#btnLoadData').click(function() {
+    console.log("clicked");
 
+    //let jsonURL = "https://www.w3schools.com/jquery/demo_ajax_json.js";
 
-     $("#noSpaces").blur(function () {
-      let inputVal = $(this).val();
-      let strSpace = " ";
-      let spaceCount = inputVal.split(" ").length - 1;
+    //let jsonURL = "demo.json";
 
-     console.log(spaceCount);
-      if (spaceCount === 0) {
-        $(this).next().text("all good");
-      } else if (spaceCount > 0) {
-        $(this).next().text("no spaces allowed in User Name");
-      }
-    });  
+    let jsonURL = "https://rish4863.github.io/indialab/";
+
+    $.ajax({
+        url: jsonURL,
+        dataType: "json",
+        success: function(result) {
+            //data = $.parseJSON(result);
+            console.log(result);
+            $.each(result, function(result) {
+                $("#dataContainer").append(result + " ");
+            });
+        }
     });
 
+    // $.ajax({
+    //     url: jsonURL,
+    //     jsonp: "callback",
+    //     dataType: "jsonp",
+    //     data: { format: "json" },
+    //     success: function(response) { console.log(response); }
+    // });
+});
+
+
+
+// end of doc ready f/n
+// });
+
+
+$(function() {
+
+
+            $('#btnLoadData').click(function() {
+                console.log("clicked");
+
+                //let jsonURL = "https://www.w3schools.com/jquery/demo_ajax_json.js";
+
+                //let jsonURL = "demo.json";
+
+                let jsonURL = "https://rish4863.github.io/indialab/";
+
+                $.ajax({
+                    url: jsonURL,
+                    dataType: "json",
+                    success: function(result) {
+                        //data = $.parseJSON(result);
+                        console.log(result);
+                        $.each(result, function(result) {
+                            $("#dataContainer").append(result + " ");
+                        });
+                    }
+                });
+
+                // $.ajax({
+                //     url: jsonURL,
+                //     jsonp: "callback",
+                //     dataType: "jsonp",
+                //     data: { format: "json" },
+                //     success: function(response) { console.log(response); }
+                // });
+            });
+
+
+
+            // end of doc ready f/n
+            // });
+   
   $("#btn").on("click", () => {
-    console.log("here");
-    let selectedSize;
-    console.log("val" + selectedSize);
-    if ($("input:radio[name='level']").is(":checked")) {
-      selectedSize = $("input[name='level']:checked").val();
-      // show the output:
+  console.log("here");
+  let selectedSize;
+  console.log("val" + selectedSize);
+  if ($("input:radio[name='level']").is(":checked")) {
+    selectedSize = $("input[name='level']:checked").val();
+    // show the output:
+  }
+  output.innerText = selectedSize
+    ? `You selected ${selectedSize}`
+    : `You haven't selected any size`;
+});
+   
+   $("#noSpaces").blur(function () {
+    let inputVal = $(this).val();
+    let strSpace = " ";
+    let spaceCount = inputVal.split(" ").length - 1;
+
+    console.log(spaceCount);
+    if (spaceCount === 0) {
+      $(this).next().text("all good");
+    } else if (spaceCount > 0) {
+      $(this).next().text("no spaces allowed in User Name");
     }
-
-// using the dependent input in relation to level and the radio button 
-
- str1 = "beginner";
- str2= "intermediate";
- str3="advanced";
- str4 = selectedSize;
-if (str1 == str4) {
-  greeting = "You are on the green team!";
-  document.getElementById("demo").innerText = greeting;
-} else  if (str2== str4) {
-  greeting = "You are on the red team!";
-  document.getElementById("demo").innerText = greeting;
-} else if (str3==str4){ 
- greeting = "You are on the blue team!"
-  document.getElementById("demo").innerText = greeting;
-}
-
+  });  
   });
 });
+
+ 
+  });
